c2d8bbd1cdabd261d562b3001e8aa31196d0775b
Héctor Palma Téllez <hector@palmatellez.cl>
2014-05-21 03:14:16 -0400

    Añadido zClip para copiar contenido de los <input>

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+    'use strict';
+
+    // Copia el contenido del input[type="text"] al clic en el input[type="submit"]
+    $('.una-formula .copiar').zclip({
+        path: 'bower_components/jquery-zclip/ZeroClipboard.swf',
+        copy: function() {
+            return $('input.snippet').val();
+        }
+    });
+
+
+});
