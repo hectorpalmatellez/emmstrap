0e95b537580605130442c2a8d5a36c6f27262052
Héctor Palma Téllez <hector@palmatellez.cl>
2014-05-21 16:17:15 -0400

    Fórmula del tooltip.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,11 +10,13 @@ $(document).ready(function() {
             $(this).attr('value', 'Copiado').addClass('disabled');
         }
     });
-    var formulas = ['button.btn.btn-primary.btn-lg[data-toggle="modal"][data-target="#elmodal"]{Abrir modal}', '.modal.fade#elmodal[tabindex="-1"][role="dialog"][aria-hidden="true"]>.modal-dialog>.modal-content>.modal-header>(button[type="button"].close[data-dismiss="modal"][aria-hidden="true"]{&times}+h4.modal-title)+^.modal-body>p^.modal-footer>button[type="button"].btn.btn-default[data-dismiss="modal"]{Close}+button[type="button"].btn.btn-primary{Save Changes}'];
+    // Las formulas
+    var formulas = ['button.btn.btn-primary.btn-lg[data-toggle="modal"][data-target="#elmodal"]{Abrir modal}', '.modal.fade#elmodal[tabindex="-1"][role="dialog"][aria-hidden="true"]>.modal-dialog>.modal-content>.modal-header>(button[type="button"].close[data-dismiss="modal"][aria-hidden="true"]{&times}+h4.modal-title)+^.modal-body>p^.modal-footer>button[type="button"].btn.btn-default[data-dismiss="modal"]{Close}+button[type="button"].btn.btn-primary{Save Changes}', 'button[type="button"].btn.btn-default[data-toggle="tooltip"][data-placement="left"][title="Tooltip on left"]{Tooltip on left}'];
 
+    // Asignar formulas a los inputs
     $('.una-formula.f-bmodal .snippet').attr('value', formulas[0]);
     $('.una-formula.f-modal .snippet').attr('value', formulas[1]);
-
+    $('.una-formula.tooltipleft .snippet').attr('value', formulas[2]);
 
     // Contador de caracteres
     $('.caracteres').each(function() {
