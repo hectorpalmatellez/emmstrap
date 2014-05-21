105b3b988ed95875e91f7c605cfde53d533937fe
Héctor Palma Téllez <hector@palmatellez.cl>
2014-05-21 19:06:49 -0400

    Added Carousel formula + minor fixes.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         }
     });
     // Las formulas
-    var formulas = ['button.btn.btn-primary.btn-lg[data-toggle="modal"][data-target="#elmodal"]{Abrir modal}', '.modal.fade#elmodal[tabindex="-1"][role="dialog"][aria-hidden="true"]>.modal-dialog>.modal-content>.modal-header>(button[type="button"].close[data-dismiss="modal"][aria-hidden="true"]{&times}+h4.modal-title)+^.modal-body>p^.modal-footer>button[type="button"].btn.btn-default[data-dismiss="modal"]{Close}+button[type="button"].btn.btn-primary{Save Changes}', 'button[type="button"].btn.btn-default[data-toggle="tooltip"][data-placement="left"][title="Tooltip on left"]{Tooltip on left}', 'button[type="button"].btn.btn-default[data-toggle="tooltip"][data-placement="right"][title="Tooltip on right"]{Tooltip on right}', 'button[type="button"].btn.btn-default[data-toggle="tooltip"][data-placement="top"][title="Tooltip on top"]{Tooltip on top}', 'button[type="button"].btn.btn-default[data-toggle="tooltip"][data-placement="bottom"][title="Tooltip on bottom"]{Tooltip on bottom}'];
+    var formulas = ['button.btn.btn-primary.btn-lg[data-toggle="modal"][data-target="#elmodal"]{Abrir modal}', '.modal.fade#elmodal[tabindex="-1"][role="dialog"][aria-hidden="true"]>.modal-dialog>.modal-content>.modal-header>(button[type="button"].close[data-dismiss="modal"][aria-hidden="true"]{&times}+h4.modal-title)+^.modal-body>p^.modal-footer>button[type="button"].btn.btn-default[data-dismiss="modal"]{Close}+button[type="button"].btn.btn-primary{Save Changes}', 'button[type="button"].btn.btn-default[data-toggle="tooltip"][data-placement="left"][title="Tooltip on left"]{Tooltip on left}', 'button[type="button"].btn.btn-default[data-toggle="tooltip"][data-placement="right"][title="Tooltip on right"]{Tooltip on right}', 'button[type="button"].btn.btn-default[data-toggle="tooltip"][data-placement="top"][title="Tooltip on top"]{Tooltip on top}', 'button[type="button"].btn.btn-default[data-toggle="tooltip"][data-placement="bottom"][title="Tooltip on bottom"]{Tooltip on bottom}', '#carousel-example.carousel.slide[data-ride="carousel"]>ol.carousel-indicators>li[data-target="#carousel-example"].active[data-slide-to="0"]+li[data-target="#carousel-example"][data-slide-to="1"]^.carousel-inner>.item.active>img[src="http://placepuppy.it/800/500"]+.carousel-caption^.item>img[src="http://placepuppy.it/800/500&n=2"]+.carousel-caption^^a.left.carousel-control[href="#carousel-example"][data-slide="prev"]>span.glyphicon.glyphicon-chevron-left^a.right.carousel-control[href="#carousel-example"][data-slide="next"]>span.glyphicon.glyphicon-chevron-right'];
 
     // Asignar formulas al value de cada input
     $('.una-formula.f-bmodal .snippet').attr('value', formulas[0]);
@@ -20,6 +20,7 @@ $(document).ready(function() {
     $('.una-formula.tooltipright .snippet').attr('value', formulas[3]);
     $('.una-formula.tooltiptop .snippet').attr('value', formulas[4]);
     $('.una-formula.tooltipbottom .snippet').attr('value', formulas[5]);
+    $('.una-formula.f-carousel .snippet').attr('value', formulas[6]);
 
 
 
